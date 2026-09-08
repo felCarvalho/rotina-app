56e66fe5a070f5979589ffd8cb16f35acde56290
felcarvalho <felcarvalho54321@gmail.com>
2026-09-08 02:38:38 -0300

    feat: atualizar interface Task com tipo Category

--- src/app/home/type.ts
+++ src/app/home/type.ts
@@ -11,10 +11,20 @@ export interface Task {
   id: string;
   title: string;
   description: string;
-  category: string;
+  category: Category;
   user: string;
   created: Date;
   updated: Date;
   deleted: Date | null;
   status: 'incompleta' | 'concluida';
+}
+
+export interface Category {
+  id: string;
+  title: string;
+  description: string;
+  user: string;
+  created: Date;
+  updated: Date;
+  deleted: Date | null;
 }
