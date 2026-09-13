93ddf906ab46bbaa5a1ef08e6b78cc885e0f0a63
felcarvalho <felcarvalho54321@gmail.com>
2026-09-12 21:05:51 -0300

    refactor: reestruturar rotas do app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,23 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('./landing-page/landing-page.component').then((m) => m.LandingPageComponent),
-  },
-  {
-    path: 'criar-conta',
-    loadChildren: () => import('./account/account.routes').then((r) => r.accountRoutes),
+      import('./landing-page/landing-page.component').then(
+        (m) => m.LandingPageComponent,
+      ),
   },
   {
     path: 'login',
-    loadChildren: () => import('./account/account.routes').then((r) => r.loginRoutes),
+    loadChildren: () => import('./auth/auth.routes').then((r) => r.loginRoutes),
+  },
+  {
+    path: 'create-account',
+    loadChildren: () =>
+      import('./account/account.routes').then((r) => r.accountRoutes),
+  },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.routes').then((r) => r.accountRoutes),
   },
   {
     path: 'home',
